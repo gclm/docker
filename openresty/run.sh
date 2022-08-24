c1f1bb6de9cece9bab842f3224b3d6e62ff17764
gclm <gclmit@163.com>
2022-08-24 12:33:18 +0800

    openresty推送镜像到dockerhub

--- openresty/run.sh
+++
@@ -1,5 +0,0 @@
-docker run -di --name openresty \
-    -p 20000:80 \
-    -v /Users/gclm/Service/Docker/openresty/test/conf:/etc/nginx/conf.d/ \
-    -v /Users/gclm/Service/Docker/openresty/test/web:/tmp/web/ \
-    openresty:1.21.4.1
